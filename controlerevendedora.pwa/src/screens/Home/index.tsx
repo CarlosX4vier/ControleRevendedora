3d59a75dc7329d469888f28bb6170be470e06ba4
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2021-11-24 16:20:37 -0300

    Adicionado PWA com bootstrap

---
+++ controlerevendedora.pwa/src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+    return <div className="container"> <span>Teste</span></div>;
+}
+
+export default Home;
