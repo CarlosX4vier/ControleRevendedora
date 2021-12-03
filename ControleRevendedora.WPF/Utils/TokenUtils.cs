4ab65ccb683ce36037cd1e4790ab856376f8f04e
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2021-12-02 22:12:11 -0300

    Adicionado injeção de dependencias

---
+++ ControleRevendedora.WPF/Utils/TokenUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace ControleRevendedora.Helpers
+{
+    public class TokenUtils
+    {
+        public static int Selecionar(int tamanho, string nomeCache)
+        {
+            MemoryCache cache = MemoryCache.Default;
+            int proximaChave = (int)(cache.Get(nomeCache) ?? 0);
+
+            if (proximaChave > tamanho - 1)
+            {
+                proximaChave = 0;
+            }
+            cache.Set(nomeCache, proximaChave + 1, new CacheItemPolicy { SlidingExpiration = TimeSpan.FromMinutes(180) });
+            return proximaChave;
+        }
+    }
+}
