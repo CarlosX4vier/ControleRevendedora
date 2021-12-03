91967ce2292720451b4850c9d4dabb63ad586345
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2021-12-02 22:11:27 -0300

    Adicionado app PWA

--- controlerevendedora.pwa/src/screens/Home/index.tsx
+++ controlerevendedora.pwa/src/screens/Home/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-
-
-// import { Container } from './styles';
+import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 
 const Home: React.FC = () => {
-    return <div className="container"> <span>Teste</span></div>;
+
+    return (
+        <div className="container">
+
+            <div className="row">
+                <div className="col-12">
+                    <h4>Boa tarde!</h4>
+                </div>
+            </div>
+
+            <div className="flex-row overflow-auto d-flex justify-content-start">
+                <div className="col-12 col-md-11 col-xl-3 card card-pink mx-2">
+                    <div className="card-body">Estoque</div>
+                </div>
+                <div className="col-12 col-md-11 col-xl-3 card card-pink mx-2">
+                    <div className="card-body">Estoque</div>
+                </div>
+            </div>
+
+
+            <div className="row">
+                <div className="card">
+
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default Home;
