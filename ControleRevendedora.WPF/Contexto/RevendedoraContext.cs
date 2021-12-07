56f3b25b0522113b33c0d61b08b236574b40c8b7
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2021-12-07 09:33:09 -0300

    Conversao de chave primaria de produto para long

--- ControleRevendedora.WPF/Contexto/RevendedoraContext.cs
+++ ControleRevendedora.WPF/Contexto/RevendedoraContext.cs
@@ -14,19 +14,25 @@ namespace ControleRevendedora.Contexto
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Transacao> Transacoes { get; set; }
         public DbSet<Marca> Marcas { get; set; }
-
+        public DbSet<Kit> Kits { get; set; }
         public RevendedoraContext() : base(ConfigurationManager.ConnectionStrings["server"].ConnectionString)
         {
-        
+            this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Transacao>()//.HasKey(a => new { a.Id, a.ProdutoID })
-                                            .HasRequired<Produto>(s => s.Produto)
+                                            .HasRequired<ProdutoBase>(s => s.Produto)
                                             .WithMany(g => g.Transacoes)
                                             .HasForeignKey(a => a.ProdutoID);
- 
+
+            modelBuilder.Entity<Kit>().HasMany<Produto>(x => x.KitProdutos)
+                                      .WithMany(x => x.Kits);
+
+            modelBuilder.Entity<ProdutoBase>().ToTable("Produtoes");
+
             base.OnModelCreating(modelBuilder);
         }
 
