8f2a9c61e88cd5c779b8c3c07104a50f3690c8b5
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2021-12-07 15:41:32 -0300

    Adaptado contexto para EntityFramework Core

---
+++ ControleRevendedora.WPF/Extensoes/DbContextExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System.IO;
+
+namespace ControleRevendedora.Extensoes
+{
+    public static class DbContextExtensions
+    {
+
+        public static string GetConnectionString(this DbContext context, string nome)
+        {
+
+            string pathToContentRoot = Directory.GetCurrentDirectory();
+            string json = Path.Combine(pathToContentRoot, "App.config");
+
+            if (!File.Exists(json))
+            {
+                string pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            }
+
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(pathToContentRoot)
+                .AddXmlFile("App.config");
+
+            IConfiguration configuration = configurationBuilder.Build();
+            var conexao = configuration.GetSection($"connectionStrings:add:{nome}:connectionString").Value;
+
+            return conexao;
+        }
+    }
+}
