d62541190330a8068a12f8a07363b05eb1de2ad2
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2021-12-09 01:28:26 -0300

    Criado ViewModelBase e implementado no CriarKitVM

--- ControleRevendedora.WPF/ViewModels/Kits/CriarKitVM.cs
+++ ControleRevendedora.WPF/ViewModels/Kits/CriarKitVM.cs
@@ -1,11 +1,13 @@
 ﻿using ControleRevendedora.Contexto;
 using ControleRevendedora.Modelos;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace ControleRevendedora.ViewModels.Kits
 {
-    public class CriarKitVM
+    public class CriarKitVM :ViewModelBase
     {
         private RevendedoraContext contexto;
         public List<Produto> Produtos { get; set; }
@@ -16,7 +18,17 @@ namespace ControleRevendedora.ViewModels.Kits
             contexto = new RevendedoraContext();
 
             Produtos = contexto.Produtos.ToList();
+        }
 
+
+        public async System.Threading.Tasks.Task<List<Produto>> ProcurarProdutos(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+                Produtos = await contexto.Produtos.Where(p => p.Nome.Contains(nome)).ToListAsync();
+            else
+                Produtos = await contexto.Produtos.ToListAsync();
+            OnPropertyChanged(nameof(this.Produtos));
+            return Produtos;
         }
     }
 }
