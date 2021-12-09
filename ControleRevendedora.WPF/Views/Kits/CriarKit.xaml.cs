6aded304246fd0e19c815f4b4f05911c3df4500e
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2021-12-09 01:27:49 -0300

    Melhorias no layout para criarKit

--- ControleRevendedora.WPF/Views/Kits/CriarKit.xaml.cs
+++ ControleRevendedora.WPF/Views/Kits/CriarKit.xaml.cs
@@ -1,5 +1,8 @@
-﻿using ControleRevendedora.ViewModels.Kits;
+﻿using ControleRevendedora.Relatorios;
+using ControleRevendedora.ViewModels.Kits;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace ControleRevendedora.Views.Kits
 {
@@ -13,6 +16,32 @@ namespace ControleRevendedora.Views.Kits
         {
             InitializeComponent();
             DataContext = VM;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            VM.Kit.KitProdutos.Remove((Modelos.Produto)dgProdutos.SelectedValue);
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            VM.Kit.KitProdutos.Add((Modelos.Produto)dgProdutos.SelectedValue);
+            ExportarKits exportar = new ExportarKits();
+            exportar.Show();
+            PrintDialog printDialog = new PrintDialog();
+            var printDialogResultado = printDialog.ShowDialog();
+            if (printDialogResultado.HasValue && printDialogResultado.Value)
+            {
+                printDialog.PrintDocument(exportar.idpSource.DocumentPaginator, "teste");
+
+
+            }
+        }
+
+        private void TxtPesquisa_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var nome = txtPesquisa.Text;
+            Task.Run(async () => { await VM.ProcurarProdutos(nome); });
         }
     }
 }
