f35b932babc013bb53ba879e0a6b70fa7c74c4a1
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2021-12-15 23:52:57 -0300

    Implementação de novos metodos para TableExtensions

--- ControleRevendedora.WPF/Extensoes/TableExtensions.cs
+++ ControleRevendedora.WPF/Extensoes/TableExtensions.cs
@@ -1,9 +1,19 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace ControleRevendedora.Extensoes
 {
+    public class TabelaCelula
+    {
+        public object Conteudo { get; set; }
+        public FontWeight? FontWeights { get; set; }
+        public Thickness? Padding { get; set; }
+        public double FontSize { get; set; } = 12;
+        public string BorderColor { get; set; } = "#000";
+    }
+
     public static class TableExtensions
     {
         public static void InicializarTable(this Table tabela)
@@ -18,7 +28,7 @@ namespace ControleRevendedora.Extensoes
                 tabela.Columns.Add(new TableColumn());
             }
         }
-        public static void AdicionarColuna(this Table tabela, double? largura = null, int grupoLinha = 0)
+        public static void AdicionarColuna(this Table tabela, double? largura = null)
         {
             var GridLengthLargura = largura.HasValue ? new GridLength(largura.Value, GridUnitType.Star) : GridLength.Auto;
 
@@ -34,18 +44,68 @@ namespace ControleRevendedora.Extensoes
             FontWeight? fontWeights = null, Thickness? padding = null)
         {
             var linha = new TableRow();
+
             foreach (object valor in valores)
             {
-                var celula = new TableCell(new Paragraph(new Run
+                Block celulaConteudo;
+
+                if (valor.GetType().IsPrimitive || valor.GetType() == typeof(string))
                 {
-                    Text = valor.ToString(),
-                    FontWeight = fontWeights ?? FontWeights.Regular,
-                }));
+                    celulaConteudo = new Paragraph(new Run
+                    {
+                        Text = valor.ToString(),
+                        FontWeight = fontWeights ?? FontWeights.Regular,
+                    });
+                }
+                else
+                {
+                    var paragrafo = new Paragraph();
+                    paragrafo.Inlines.Add((UIElement)valor);
+                    celulaConteudo = paragrafo;
+                }
+
+                var celula = new TableCell(celulaConteudo);
 
                 if (padding.HasValue)
                     celula.Padding = padding.Value;
                 celula.BorderThickness = new Thickness(1);
                 celula.BorderBrush = (Brush)new BrushConverter().ConvertFrom("#000");
+
+                linha.Cells.Add(celula);
+            }
+
+            tabela.RowGroups[grupoLinha].Rows.Add(linha);
+        }
+
+        public static void AdicionarLinha(this Table tabela, TabelaCelula[] celulas, int grupoLinha = 0)
+        {
+            var linha = new TableRow();
+
+            foreach (TabelaCelula valor in celulas)
+            {
+                Block celulaConteudo;
+                if (valor.Conteudo.GetType().IsPrimitive || valor.Conteudo.GetType() == typeof(string))
+                {
+                    celulaConteudo = new Paragraph(new Run
+                    {
+                        Text = valor.Conteudo.ToString(),
+                        FontSize = valor.FontSize,
+                        FontWeight = valor.FontWeights ?? FontWeights.Regular,
+                    });
+                }
+                else
+                {
+                    var paragrafo = new Paragraph();
+                    paragrafo.Inlines.Add((UIElement)valor.Conteudo);
+                    celulaConteudo = paragrafo;
+                }
+
+                var celula = new TableCell(celulaConteudo);
+
+                if (valor.Padding.HasValue)
+                    celula.Padding = valor.Padding.Value;
+                celula.BorderThickness = new Thickness(1);
+                celula.BorderBrush = (Brush)new BrushConverter().ConvertFrom(valor.BorderColor);
 
                 linha.Cells.Add(celula);
             }
